8221e996403dce82b1764c2ef8aa7df77303bbea
Mai (Mabel) Nguyen <mabel060603@gmail.com>
2024-03-09 22:44:45 +1030

    hash password

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,4 +1,5 @@
 import User from '../models/user.model.js'
+import bcryptjs from 'bcryptjs'
 
 export const signup = async (req, res) => {
     const { username, email, password } = req.body;
@@ -7,17 +8,20 @@ export const signup = async (req, res) => {
         return res.status(400).json({ message: 'All fields are required' })
     }
 
+    // Hash password
+    const hashedPassword = bcryptjs.hashSync(password, 10)
+
     const newUser = new User({
         username,
         email,
-        password,
+        password: hashedPassword,
     })
     try {
 
         // Save user to database
         await newUser.save()
         res.json({ message: 'Sign up successful' })
-        
+
     } catch (error) {
         res.status(500).json({ message: error.message })
     }
