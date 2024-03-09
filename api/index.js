9d752782f3e6cdc021ba7c831edb6642334b184f
Mai (Mabel) Nguyen <mabel060603@gmail.com>
2024-03-09 20:38:37 +1030

    add mongodb database connection

--- api/index.js
+++ api/index.js
@@ -1,5 +1,17 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
 
+dotenv.config()
+
+mongoose.connect(process.env.MONGO_URL)
+    .then(() => {
+            console.log('MongoDB is connected')
+    })
+    .catch((error) => {
+        console.log(error.message)
+    })
+// SWEmH7M9LNDgoYm0
 const app = express()
 
 app.listen(3000, () => {
