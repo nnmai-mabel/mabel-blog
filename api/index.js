8bbb0e1b20df6c9d65835c23cd009f2418e51e57
Mai (Mabel) Nguyen <mabel060603@gmail.com>
2024-03-16 14:57:41 +1030

    add the comment section to post page

--- api/index.js
+++ api/index.js
@@ -3,8 +3,9 @@ import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import userRoutes from './routes/user.route.js'
 import authRoutes from './routes/auth.route.js'
-import cookieParser from 'cookie-parser'
 import postRoutes from './routes/post.route.js'
+import commentRoutes from './routes/comment.route.js'
+import cookieParser from 'cookie-parser'
 
 dotenv.config()
 
@@ -29,6 +30,7 @@ app.listen(3000, () => {
 app.use('/api/user', userRoutes)
 app.use('/api/auth', authRoutes)
 app.use('/api/post', postRoutes)
+app.use('/api/comment', commentRoutes)
 
 // Middleware
 app.use((err, req, res, next) => {
