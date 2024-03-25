cc1134c54ce6ece2acefef91ce87a0e3d8c31f78
Mai (Mabel) Nguyen <137015107+nnmai-mabel@users.noreply.github.com>
2024-03-25 15:45:02 +1030

    Update CallToAction.jsx

--- client/src/components/CallToAction.jsx
+++ client/src/components/CallToAction.jsx
@@ -5,14 +5,14 @@ export default function CallToAction() {
     <div className="flex flex-col sm:flex-row p-3 border border-cyan-500 justify-center items-center rounded-tl-3xl rounded-br-3xl text-center">
         <div className="flex-1 justify-center flex flex-col">
             <h2 className="text-2xl">
-                Want to learn more about ReactJS?
+                Want to learn more about IT?
             </h2>
             <p className="text-gray-500 my-2">
                 Check out these resources
             </p>
             <Button gradientMonochrome='info' className="rounded-tl-xl rounded-bl-none">
-                <a href="#" target="_blank" rel="noopener noreferrer">
-                    100 Projects
+                <a href="https://mabel-blog.onrender.com/" target="_blank" rel="noopener noreferrer">
+                    Mabel's blog
                 </a>
             </Button>
         </div>
